715c79d6efb12554b40810b6ce70ed1bc8a307f1
circumF1ex <91390208+circumF1ex@users.noreply.github.com>
2021-12-20 21:32:04 +0300

    Add files via upload
    nothing special

---
+++ Interface1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    interface ITeacher
+    {
+        void SetPost(TPost Post);
+        string TAction();
+    }
+
+    interface IStudent
+    {
+        string SAction();
+    }
+    interface IJobHunter
+    {
+        string JAction1(string FIO, TPost Post);
+        string JAction2(string FIO, int value);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School.jobhunters.Add(new School.JobHunter("Создан кадровик - Паннакотта Георгий Викторович", JPost.JobHunter));
+            Console.WriteLine($"{School.jobhunters[0].FIO}: {School.jobhunters[0].Post}");
+            
+            School.jobhunters.Add(new School.JobHunter("Создан кадровик - Тирамису Мария Нафановна", JPost.JobHunter));
+            Console.WriteLine($"{School.jobhunters[1].FIO}: {School.jobhunters[1].Post}");
+
+            School.jobhunters[0].JAction1("Танненбаум Виктор Юрьевич", TPost.Assistant);
+            Console.WriteLine(School.jobhunters[0].message);
+
+            School.jobhunters[1].JAction1("Силенок Юрий Викторович", TPost.Senior);
+            Console.WriteLine(School.jobhunters[1].message);
+
+            Console.WriteLine(School.teachers[0].TAction());
+            Console.WriteLine(School.teachers[1].TAction());
+
+            School.jobhunters[0].JAction2("Сизов Данил Евгеньевич", 2);
+            Console.WriteLine(School.students[0].SAction());
+
+            Console.ReadLine();
+        }
+    }
+}
